fcf662a1f3af33af23681f75f684b41574257377
riccardo borroni <riccardoborroni21@gmail.com>
2025-10-31 16:33:19 +0100

    change login from post to get

--- dist/routes/auth/loginRoute.js
+++ dist/routes/auth/loginRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { loginController } from "../../controllers/loginControllers.js";
 const router = express.Router();
-router.post("/", loginController);
+router.get("/", loginController);
 export default router;
 //# sourceMappingURL=loginRoute.js.map
