bdeccd9a19f72d32179d9907de87680dd5af25f8
riccardo borroni <riccardoborroni21@gmail.com>
2025-10-31 16:50:21 +0100

    build prova 1

--- dist/controllers/loginControllers.js
+++ dist/controllers/loginControllers.js
@@ -1,5 +1,4 @@
 import sendRequest from "../utils/sendRequest.js";
-import calcolaMedia from "../utils/media.js";
 export const loginController = async (req, res) => {
     try {
         const body = req.body;
@@ -9,6 +8,7 @@ export const loginController = async (req, res) => {
             pass: "Rickardo@07",
             app_code: "CVVS",
         });
+        console.log(req.body);
         console.log(result);
         // Verifica che data esista
         if (!result.data) {
@@ -16,38 +16,44 @@ export const loginController = async (req, res) => {
                 message: result.message || "data is null",
             });
         }
-        // Richiesta dei voti
-        const gradesResult = await sendRequest(`students/${result.data.ident.substring(1)}/grades`, "GET", null, result.data.token);
-        if (!gradesResult.data ||
-            !Array.isArray(gradesResult.data.grades) ||
-            gradesResult.data.grades.length === 0) {
-            return res.status(404).json({
-                message: "grades not found",
-            });
-        }
-        // Aggiorna la proprietà class con sicurezza
-        const userData = result.data;
-        userData.lastMarks =
-            gradesResult.data?.grades?.[0]?.displayValue ?? "N/A";
-        let subject;
-        const str = gradesResult.data.grades[0].subjectDesc.trim().split(/\s+/);
-        if (str.length > 1) {
-            subject = str[0] + "...";
-        }
-        else
-            subject = str[0];
-        userData.lastSubject = subject ? subject : "N/A";
-        const mark = gradesResult.data?.grades?.[0]?.decimalValue ?? "a";
-        userData.marksColor =
-            mark == "a"
-                ? "#18b2ff5c"
-                : mark >= 5 && mark < 6
-                    ? "#ff7418aa"
-                    : mark < 5
-                        ? "#ff4d4da2"
-                        : "#3ce339aa";
-        userData.mediaVoti =
-            Math.round(calcolaMedia(gradesResult.data) * 10) / 10;
+        // // Richiesta dei voti
+        // const gradesResult = await sendRequest<Grades>(
+        // 	`students/${result.data.ident.substring(1)}/grades`,
+        // 	"GET",
+        // 	null,
+        // 	result.data.token
+        // );
+        // if (
+        // 	!gradesResult.data ||
+        // 	!Array.isArray(gradesResult.data.grades) ||
+        // 	gradesResult.data.grades.length === 0
+        // ) {
+        // 	return res.status(404).json({
+        // 		message: "grades not found",
+        // 	});
+        // }
+        // // Aggiorna la proprietà class con sicurezza
+        // const userData: User = result.data;
+        // userData.lastMarks =
+        // 	gradesResult.data?.grades?.[0]?.displayValue ?? "N/A";
+        // let subject;
+        // const str =
+        // 	gradesResult.data.grades[0]!.subjectDesc.trim().split(/\s+/);
+        // if (str.length > 1) {
+        // 	subject = str[0] + "...";
+        // } else subject = str[0];
+        // userData.lastSubject = subject ? subject : "N/A";
+        // const mark = gradesResult.data?.grades?.[0]?.decimalValue ?? "a";
+        // userData.marksColor =
+        // 	mark == "a"
+        // 		? "#18b2ff5c"
+        // 		: mark >= 5 && mark < 6
+        // 		? "#ff7418aa"
+        // 		: mark < 5
+        // 		? "#ff4d4da2"
+        // 		: "#3ce339aa";
+        // userData.mediaVoti =
+        // 	Math.round(calcolaMedia(gradesResult.data) * 10) / 10;
         return res.status(result.status).json(result);
     }
     catch (error) {
